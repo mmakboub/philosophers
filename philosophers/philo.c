e9a19fcd1c1d87b7244b9c7faf4338219088426f
Mariam Makboub <mmakboub@e1r7p7.1337.ma>
2022-09-17 23:48:20 +0100

    ok

--- philosophers/philo.c
+++ philosophers/philo.c
@@ -6,21 +6,30 @@
 /*   By: mmakboub <mmakboub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 16:27:57 by mmakboub          #+#    #+#             */
-/*   Updated: 2022/09/17 23:17:03 by mmakboub         ###   ########.fr       */
+/*   Updated: 2022/09/17 23:47:55 by mmakboub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"philosophers.h"
-
+void    creatthreads(t_compstargs   *argphilo)
+{
+    int i;
+    i = 0;
+    while(i < argphilo-> nbr_philo)
+    {
+        i++;//i is the index of philo
+    }
+    
+}
 int main(int ac, char **av)
 {
     t_compstargs    argphilo;
-    // if(ac != 6 && ac != 5)
-    //     errorinargument();
+    
     if(ac == 5 || ac == 6)
     {
         check_digit(av);
         initialiaze_argv(&argphilo, av, ac);
+        creat_threads(&argphilo)
     }
     else
         errorinargument();
